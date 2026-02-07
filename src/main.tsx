ebf5f4ca57477677a6c7a6690bbcebbd7d592f10
Andreas Franson <andreas@internetintelligence.se>
2026-02-07 12:30:28 -0500

    Unregister service worker

--- src/main.tsx
+++ src/main.tsx
@@ -19,10 +19,12 @@ createRoot(document.getElementById('root')!).render(
   </StrictMode>
 )
 
-// Keep your SW registration
+// 🚨 Disable PWA / Service Worker (maintenance mode)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.getRegistrations().then((regs) => {
+      regs.forEach((reg) => reg.unregister())
+    })
   })
 }
 
