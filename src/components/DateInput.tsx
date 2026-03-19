f9adb4c9efc8723c129ffffb17b8432762a5326f
Andreas Franson <andreas@internetintelligence.se>
2026-03-18 23:56:22 -0400

    Fixing date format on all pages (tenants locale)_2

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -55,7 +55,7 @@ export function DateInput({ value, onChange, style, className, disabled, ...rest
         style={{
           position: 'absolute', top: 0, left: 0,
           width: '100%', height: '100%',
-          opacity: 0, pointerEvents: disabled ? 'none' : 'all',
+          opacity: 0, pointerEvents: 'none',
           cursor: 'pointer',
         }}
         {...rest}
